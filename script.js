697dcdb0ca6b23b14777452c4b17afb0938dbba0
gruen <158203519+gruensys@users.noreply.github.com>
2024-04-06 00:43:37 +0200

    Aktualisieren von script.js

--- script.js
+++ script.js
@@ -9,50 +9,41 @@ window.onresize = resizeWallpaper;
 
 function openApp(appName) {
     const windowElement = document.getElementById(appName + 'App');
-    const iframe = windowElement.querySelector('iframe');
-
-    if (!iframe) {
-        const newIframe = document.createElement('iframe');
-        newIframe.src = getAppURL(appName);
-        newIframe.style.width = '100%';
-        newIframe.style.height = '100%';
-        windowElement.appendChild(newIframe);
-    }
-
     windowElement.classList.add('active');
-    centerWindow(windowElement);
+    if (!windowElement.classList.contains('maximized')) {
+        const centerX = (window.innerWidth - windowElement.offsetWidth) / 2;
+        const centerY = (window.innerHeight - windowElement.offsetHeight) / 2;
+        windowElement.style.left = centerX + 'px';
+        windowElement.style.top = centerY + 'px';
+    }
     $(windowElement).draggable();
 }
 
 function closeApp(appName) {
     const windowElement = document.getElementById(appName + 'App');
     windowElement.classList.remove('active');
+    const iframe = windowElement.querySelector('iframe');
+    iframe.src = iframe.src;
 }
 
 function minimizeApp(appName) {
-    const app = document.getElementById(appName + 'App');
+    var app = document.getElementById(appName + 'App');
     app.classList.add('minimized');
 }
 
 function maximizeApp(appName) {
     const app = document.getElementById(appName + 'App');
-    app.classList.toggle('maximized');
-    if (app.classList.contains('maximized')) {
-        app.style.width = "100%";
-        app.style.height = "100%";
+    if (!app.classList.contains('maximized')) {
+        app.style.width = "100vw";
+        app.style.height = "100vh";
         app.style.top = "0";
         app.style.left = "0";
+        app.classList.add('maximized');
     } else {
-        centerWindow(app);
+        app.style.width = "";
+        app.style.height = "";
+        app.style.top = "";
+        app.style.left = "";
+        app.classList.remove('maximized');
     }
-}
-
-function getAppURL(appName) {
-}
-
-function centerWindow(windowElement) {
-    const centerX = (window.innerWidth - windowElement.offsetWidth) / 2;
-    const centerY = (window.innerHeight - windowElement.offsetHeight) / 2;
-    windowElement.style.left = centerX + 'px';
-    windowElement.style.top = centerY + 'px';
 }
