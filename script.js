14fc52246d371ac720b1e6012abf71a9b408e3a5
gruen <158203519+gruensys@users.noreply.github.com>
2024-04-06 01:21:10 +0200

    Aktualisieren von script.js

--- script.js
+++ script.js
@@ -17,10 +17,10 @@ function openApp(appName) {
     windowElement.classList.add('active');
     if (!windowElement.classList.contains('maximized')) {
         centerWindow(windowElement);
-        if (appName === 'game') {
-            const iframe = windowElement.querySelector('iframe');
-            iframe.src += '&autoplay=1'; 
-        }
+    }
+    if (appName === 'game') {
+        const iframe = windowElement.querySelector('iframe');
+        iframe.src = 'https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1'; 
     }
     $(windowElement).draggable();
     $(windowElement).fadeIn();
@@ -30,7 +30,11 @@ function closeApp(appName) {
     const windowElement = document.getElementById(appName + 'App');
     windowElement.classList.remove('active');
     const iframe = windowElement.querySelector('iframe');
-    iframe.src = iframe.src;
+    if (appName === 'game') {
+        iframe.src = ''; // Reset iframe source to stop video
+    } else {
+        iframe.src = iframe.src; // Resetting iframe src to fix reloading issue
+    }
 }
 
 function minimizeApp(appName) {
