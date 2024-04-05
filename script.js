f7e562744eba35b586f35b59730c3d966c1a0b48
gruen <158203519+gruensys@users.noreply.github.com>
2024-04-06 00:20:27 +0200

    Aktualisieren von script.js

--- script.js
+++ script.js
@@ -9,25 +9,24 @@ window.onresize = resizeWallpaper;
 
 function openApp(appName) {
     const windowElement = document.getElementById(appName + 'App');
+    const iframe = windowElement.querySelector('iframe');
+
+    if (!iframe) {
+        const newIframe = document.createElement('iframe');
+        newIframe.src = getAppURL(appName);
+        newIframe.style.width = '100%';
+        newIframe.style.height = '100%';
+        windowElement.appendChild(newIframe);
+    }
+
     windowElement.classList.add('active');
-
-    const centerX = (window.innerWidth - windowElement.offsetWidth) / 2;
-    const centerY = (window.innerHeight - windowElement.offsetHeight) / 2;
-    windowElement.style.left = centerX + 'px';
-    windowElement.style.top = centerY + 'px';
-
+    centerWindow(windowElement);
     $(windowElement).draggable();
 }
 
 function closeApp(appName) {
     const windowElement = document.getElementById(appName + 'App');
     windowElement.classList.remove('active');
-    
- 
-    const iframe = windowElement.querySelector('iframe');
-    if (iframe) {
-        iframe.parentNode.removeChild(iframe);
-    }
 }
 
 function minimizeApp(appName) {
@@ -44,11 +43,16 @@ function maximizeApp(appName) {
         app.style.top = "0";
         app.style.left = "0";
     } else {
-        const centerX = (window.innerWidth - app.offsetWidth) / 2;
-        const centerY = (window.innerHeight - app.offsetHeight) / 2;
-        app.style.width = "";
-        app.style.height = "";
-        app.style.top = centerY + 'px';
-        app.style.left = centerX + 'px';
+        centerWindow(app);
     }
+}
+
+function getAppURL(appName) {
+}
+
+function centerWindow(windowElement) {
+    const centerX = (window.innerWidth - windowElement.offsetWidth) / 2;
+    const centerY = (window.innerHeight - windowElement.offsetHeight) / 2;
+    windowElement.style.left = centerX + 'px';
+    windowElement.style.top = centerY + 'px';
 }
