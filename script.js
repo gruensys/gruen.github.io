e91f26aa9c1d3f186e5fbee225a5235cd083cc38
gruen <158203519+gruensys@users.noreply.github.com>
2024-04-06 11:55:32 +0200

    Aktualisieren von script.js

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ function centerWindow(windowElement) {
     windowElement.style.top = Math.max(centerY, 0) + 'px';
 }
 
-window.addEventListener('resize', function() {
+window.addEventListener('resize', function () {
     const activeWindows = document.querySelectorAll('.window.active');
-    activeWindows.forEach(function(windowElement) {
+    activeWindows.forEach(function (windowElement) {
         centerWindow(windowElement);
     });
 });
@@ -18,49 +18,28 @@ function openApp(appName) {
     if (!windowElement.classList.contains('maximized')) {
         centerWindow(windowElement);
     }
-    if (appName === 'game') {
-        const iframe = windowElement.querySelector('iframe');
-        iframe.src = 'https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1'; 
-    }
-    $(windowElement).draggable();
-    $(windowElement).fadeIn();
 }
 
 function closeApp(appName) {
     const windowElement = document.getElementById(appName + 'App');
     windowElement.classList.remove('active');
-    const iframe = windowElement.querySelector('iframe');
-    if (appName === 'game') {
-        iframe.src = ''; // Reset iframe source to stop video
-    } else {
-        iframe.src = iframe.src; // Resetting iframe src to fix reloading issue
-    }
 }
 
-function minimizeApp(appName) {
-    const windowElement = document.getElementById(appName + 'App');
-    if (windowElement.classList.contains('maximized')) {
-        windowElement.classList.add('minimized');
-    }
-}
+document.querySelectorAll('.icon').forEach(function (icon) {
+    icon.addEventListener('click', function () {
+        const appName = this.dataset.appName;
+        openApp(appName);
+    });
+});
 
-function maximizeApp(appName) {
+function toggleSize(appName) {
     const windowElement = document.getElementById(appName + 'App');
-    if (!windowElement.classList.contains('maximized')) {
-        windowElement.style.width = "100vw";
-        windowElement.style.height = "100vh";
-        windowElement.style.top = "0";
-        windowElement.style.left = "0";
-        windowElement.style.zIndex = "2";
+    if (!document.fullscreenElement) {
         windowElement.classList.add('maximized');
+        windowElement.style.left = '40px';
+        windowElement.style.top = '40px';
     } else {
-        windowElement.style.width = "";
-        windowElement.style.height = "";
-        windowElement.style.top = "";
-        windowElement.style.left = "";
         windowElement.classList.remove('maximized');
-        if (!windowElement.classList.contains('minimized')) {
-            centerWindow(windowElement);
-        }
+        centerWindow(windowElement);
     }
 }
